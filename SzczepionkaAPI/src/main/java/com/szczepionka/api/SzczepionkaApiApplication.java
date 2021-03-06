eb5e1b591e305030ad37d48a5fb088fdeb5d6bf1
Bartek <239034@edu.p.lodz.pl>
2021-03-06 23:41:14 +0100

    Init commit for Backend

---
+++ SzczepionkaAPI/src/main/java/com/szczepionka/api/SzczepionkaApiApplication.java
@@ -0,0 +1,13 @@
+package com.szczepionka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzczepionkaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzczepionkaApiApplication.class, args);
+	}
+
+}
