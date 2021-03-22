fb1c002eb377ce112b02773e8d966dfbc8af521d
Kamil <kamil.jurek@yahoo.pl>
2021-03-22 21:09:13 +0100

    Added prototype version of landing page with redirect to registration page.

--- SzczepionkaWEB/src/app/app-routing.module.ts
+++ SzczepionkaWEB/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './component/register/register.component';
 import { MapComponent } from './component/map/map.component';
+import { LandingPageComponent } from "./component/landing-page/landing-page.component";
 
 const routes: Routes = [
+  {path: '', component: LandingPageComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'map', component: MapComponent}
 ];
