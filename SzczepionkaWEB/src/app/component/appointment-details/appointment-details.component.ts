4cf447f39a3097221a190f9e0d1370b3abd54f86
aksuz <zuzanna.burzynska@interia.pl>
2021-04-05 23:39:16 +0200

    appointment details view

--- SzczepionkaWEB/src/app/component/appointment-details/appointment-details.component.ts
+++ SzczepionkaWEB/src/app/component/appointment-details/appointment-details.component.ts
@@ -1,49 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { AppointmentDetailsService } from '../../service/appointment-details/appointment-details.service';
-import { AppointmentDetails } from '../../model/AppointmentDetails';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {AppointmentDetailsService} from '../../service/appointment-details/appointment-details.service';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
-  selector: 'app-appointment-details',
-  templateUrl: './appointment-details.component.html',
-  styleUrls: ['./appointment-details.component.css']
+    selector: 'app-appointment-details',
+    templateUrl: './appointment-details.component.html',
+    styleUrls: ['./appointment-details.component.css']
 })
 export class AppointmentDetailsComponent implements OnInit {
 
-  appointment: any;
-  patientUUID: any;
+    appointment: any;
+    patientUUID: any;
 
-  constructor(private appointmentService: AppointmentDetailsService, private route: ActivatedRoute) {
+    constructor(private appointmentService: AppointmentDetailsService, private route: ActivatedRoute) {
 
-  }
+    }
 
-  ngOnInit(): void {
-    this.patientUUID = this.route.snapshot.paramMap.get('patientUUID');
-    this.getAppointment();
-    //53579dae-b861-4186-826a-dc534d66e44e UUID to test component
-/*    this.appointment = new AppointmentDetails(
-      'patientReferral-123',
-      'Moderna',
-      'PLANNED',
-      '15-05-2021',
-      '8:30',
-      'Lodz',
-      '12-345',
-      'Streeeeeeet 11/223',
-      'Przychodnia XYZ');*/
-  }
+    ngOnInit(): void {
+        this.patientUUID = this.route.snapshot.paramMap.get('patientUUID');
+        this.getAppointment();
+        //53579dae-b861-4186-826a-dc534d66e44e UUID to test component
+        /*    this.appointment = new AppointmentDetails(
+              'patientReferral-123',
+              'Moderna',
+              'PLANNED',
+              '15-05-2021',
+              '8:30',
+              'Lodz',
+              '12-345',
+              'Streeeeeeet 11/223',
+              'Przychodnia XYZ');*/
+    }
 
-  getAppointment(): void {
-    this.appointmentService.getAppointment(this.patientUUID).subscribe(
-      data => {
-        this.appointment = data;
+    getAppointment(): void {
+        this.appointmentService.getAppointment(this.patientUUID).subscribe(
+            data => {
+                this.appointment = data;
 
-      }, error => {
-        console.log(error);
-      }, () => {
-        console.log(this.appointment);
-      });
-  }
+            }, error => {
+                console.log(error);
+            }, () => {
+                console.log(this.appointment);
+            });
+    }
+
+    cancelAppointment(): void {
+    }
+
+    viewVaccinationDetails(): void {
+
+    }
 
 }
