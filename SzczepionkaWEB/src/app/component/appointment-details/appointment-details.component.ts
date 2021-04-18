0c6b28ae16efdc726caa0f3889928fa642b212e8
Bartek <239034@edu.p.lodz.pl>
2021-04-18 23:38:38 +0200

    Extended Appointment Details - connected with backend

--- SzczepionkaWEB/src/app/component/appointment-details/appointment-details.component.ts
+++ SzczepionkaWEB/src/app/component/appointment-details/appointment-details.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { AppointmentDetailsService } from '../../service/appointment-details/appointment-details.service';
 import { ActivatedRoute, } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
+import { AppointmentDetails } from '../../model/AppointmentDetails';
 
 @Component({
   selector: 'app-appointment-details',
@@ -10,9 +11,17 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppointmentDetailsComponent implements OnInit {
 
-  appointment: any;
+  appointment!: AppointmentDetails;
   patientUUID: any;
-  isSecondVisitExist = true;
+  isSecondAppointmentExist = false;
+
+  appointmentStatusMapping = new Map([
+    ['PLANNED', 'ZAPLANOWANA'],
+    ['CANCELLED', 'ODWOŁANA'],
+    ['POSTPONED', 'PRZEŁOŻONA'],
+    ['NOT_PLANNED', 'NIEZAPLANOWANA'],
+    ['DONE', 'ZREALIZOWANA'],
+  ]);
 
   constructor(private appointmentService: AppointmentDetailsService,
               private route: ActivatedRoute,
@@ -32,38 +41,43 @@ export class AppointmentDetailsComponent implements OnInit {
       }, error => {
         console.log(error);
       }, () => {
+        if (this.appointment.secondAppointmentDate){
+          this.isSecondAppointmentExist = true;
+        }
       });
   }
 
   private replaceStatusDescription(): void {
-    if (this.appointment.appointmentStatus === 'PLANNED') {
-      this.appointment.appointmentStatus = 'ZAPLANOWANA';
-    } else if (this.appointment.appointmentStatus === 'CANCELLED') {
-      this.appointment.appointmentStatus = 'ODWOŁANA';
-    } else if (this.appointment.appointmentStatus === 'POSTPONED') {
-      this.appointment.appointmentStatus = 'PRZEŁOŻONA';
-    } else if (this.appointment.appointmentStatus === 'NOT_PLANNED') {
-      this.appointment.appointmentStatus = 'NIE ZAPLANOWANA';
-    } else if (this.appointment.appointmentStatus === 'placeholder') {
-      this.appointment.appointmentStatus = 'ZREALIZOWANA';
+    for (const [key, value] of this.appointmentStatusMapping) {
+      if (this.appointment.firstAppointmentStatus === key) {
+        this.appointment.firstAppointmentStatus = value;
+      }
+
+      if (this.appointment.secondAppointmentStatus === key) {
+        this.appointment.secondAppointmentStatus = value;
+      }
     }
   }
 
   cancelAppointment(): void {
-    if (confirm('Czy na pewno chcesz odwołać wizytę?')) {
-      this.appointmentService.cancelAppointment(this.appointment.appointmentId).subscribe(
-        data => {
-        }, error => {
-          console.log(error);
-        }, () => {
-          window.location.reload();
-        });
+    if (confirm('Czy na pewno chcesz odwołać pierwszą wizytę?')) {
+      if (this.appointment.secondAppointmentStatus) {
+        alert('Nie możesz odwołać wizyty, gdy druga wizyta została już ustalona.');
+      } else {
+        this.appointmentService.cancelAppointment(1, this.appointment.appointmentId).subscribe(
+          data => {
+          }, error => {
+            console.log(error);
+          }, () => {
+            window.location.reload();
+          });
+      }
     }
   }
 
   viewVaccinationDetails(): void {
     if (this.appointment.vaccinationBrandt === 'ASTRA_ZENECA') {
-      this.document.location.href = 'https://www.astrazeneca.pl/content/dam/az-pl/ulotki-lekow/PIL_Vaxzevria_2021-03-23.pdf';
+      this.document.location.href = 'https://ec.europa.eu/health/documents/community-register/2021/20210129150842/anx_150842_pl.pdf';
     } else if (this.appointment.vaccinationBrandt === 'PFIZER') {
       this.document.location.href = 'http://zozmswia.gda.pl/wp-content/uploads/Comirnaty-PIL-Poland_21_01_08.pdf';
     } else if (this.appointment.vaccinationBrandt === 'MODERNA') {
@@ -72,10 +86,26 @@ export class AppointmentDetailsComponent implements OnInit {
   }
 
   secondAppointment(): void {
-
+    this.appointmentService.newSecondAppointment(this.appointment.appointmentId).subscribe(
+      data => {
+      }, error => {
+        console.log(error);
+      }, () => {
+        this.getAppointment();
+        window.location.reload();
+      });
   }
 
-  cancelSecondAppointment(): void{
+  cancelSecondAppointment(): void {
+    if (confirm('Czy na pewno chcesz odwołać drugą wizytę?')) {
+      this.appointmentService.cancelAppointment(2, this.appointment.appointmentId).subscribe(
+        data => {
+        }, error => {
+          console.log(error);
+        }, () => {
+          window.location.reload();
+        });
+    }
 
   }
 
